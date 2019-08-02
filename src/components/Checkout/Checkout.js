bed62105e775d3216eebdf8628fbab31da4c39e1
Hector Valles <hmvalles75@yahoo.com>
2019-08-02 10:09:28 -0700

    inital setup

---
+++ src/components/Checkout/Checkout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Total from "./Total/Total";
+import CheckoutFeature from "./CheckoutFeature/CheckoutFeature";
+
+function Checkout(props) {
+  return (
+    <section className="main__summary">
+      <h3>NEW GREENLEAF 2018</h3>
+      <CheckoutFeature selected={props.selected} />
+      <Total selected={props.selected} />
+    </section>
+  );
+}
+
+export default Checkout;
